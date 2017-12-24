ce813406231d7ce23b48d8f9c600203844a57245
Garrett-Thomas <league@WTS5.attlocal.net>
2017-12-24 13:00:47 -0800

    adssad
    d

--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -1,7 +1,50 @@
 package threads;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String[] args) {
+		Robot a = new Robot(460, 500);
+		Robot b = new Robot(380, 450);
+		Robot c = new Robot(560, 500);
+		Robot d = new Robot(500, 450);
+		Robot e = new Robot(640, 450);
+
+		new Thread(() -> {
+			a.penDown();
+			for (int i = 0; i < 360; i++) {
+				a.move(1);
+				a.turn(1);
+			}
+		}).start();
+		new Thread(() -> {
+			b.penDown();
+			for (int i = 0; i < 360; i++) {
+				b.move(1);
+				b.turn(1);
+			}
+		}).start();
+		new Thread(() -> {
+			c.penDown();
+			for (int i = 0; i < 360; i++) {
+				c.move(1);
+				c.turn(1);
+			}
+		}).start();
+		new Thread(() -> {
+			d.penDown();
+			for (int i = 0; i < 360; i++) {
+				d.move(1);
+				d.turn(1);
+			}
+		}).start();
+		new Thread(() -> {
+			e.penDown();
+			for (int i = 0; i < 360; i++) {
+				e.move(1);
+				e.turn(1);
+			}
+		}).start();
+	}
 
 }
-
--- src/threads/ThreadedReverseGreeting.java
+++ src/threads/ThreadedReverseGreeting.java
@@ -1,7 +1,23 @@
+package threads;
 
 public class ThreadedReverseGreeting {
-  //Write a program that creates a thread (Thread 1) that creates another thread (Thread 2); 
-  //Thread 2 creates Thread 3; and so on, up to Thread 50. 
-  //Each thread should print "Hello from Thread <num>!", 
-  //but you should structure your program such that the threads print their greetings in reverse order.
+	// Write a program that creates a thread (Thread 1) that creates another thread
+	// (Thread 2);
+	// Thread 2 creates Thread 3; and so on, up to Thread 50.
+	// Each thread should print "Hello from Thread <num>!",
+	// but you should structure your program such that the threads print their
+	// greetings in reverse order.
+	public static void main(String[] args) {
+		makeThread(50);
+	}
+
+	private static void makeThread(int i) {
+		if (i > 0) {
+			new Thread(() -> {
+				makeThread(i - 1);
+			}).start();
+		}
+		System.out.println("Hello from Thread " + i);
+	}
+
 }
